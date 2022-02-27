a80733d9b33fcaa8fb297902d29cc27e83575d7b
songyinyin <song_yinyin@163.com>
2022-02-27 16:42:59 +0800

    增加提前编译 初始化SQL脚本 - 访到包里

--- src/main/java/cn/nboot/nativedemo/initdata/VersionDataSourceScriptHints.java
+++ src/main/java/cn/nboot/nativedemo/initdata/VersionDataSourceScriptHints.java
@@ -5,7 +5,7 @@ import org.springframework.nativex.hint.ResourceHint;
 import org.springframework.nativex.type.NativeConfiguration;
 
 @NativeHint(trigger = VersionDataSourceScriptDatabaseInitializer.class,
-    resources = @ResourceHint(patterns = {"db/data/*.sql", "db/schema/*.sql"}))
+    resources = @ResourceHint(patterns = {"cn/nboot/nativedemo/initdata/data/*.sql", "cn/nboot/nativedemo/initdata/schema/*.sql"}))
 public class VersionDataSourceScriptHints implements NativeConfiguration {
 
 }
