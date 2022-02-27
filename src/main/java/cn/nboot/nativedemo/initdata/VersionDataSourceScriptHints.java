5705ee2089763f8f8f35b5323b7b2055b82d6598
songyinyin <song_yinyin@163.com>
2022-02-27 15:58:46 +0800

    增加提前编译 初始化SQL脚本 - 注解形式

--- src/main/java/cn/nboot/nativedemo/initdata/VersionDataSourceScriptHints.java
+++ src/main/java/cn/nboot/nativedemo/initdata/VersionDataSourceScriptHints.java
@@ -1,24 +1,11 @@
 package cn.nboot.nativedemo.initdata;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.aot.context.bootstrap.generator.infrastructure.nativex.NativeConfigurationRegistry;
-import org.springframework.aot.context.bootstrap.generator.infrastructure.nativex.NativeResourcesEntry;
-import org.springframework.nativex.AotOptions;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.ResourceHint;
 import org.springframework.nativex.type.NativeConfiguration;
 
-@Slf4j
+@NativeHint(trigger = VersionDataSourceScriptDatabaseInitializer.class,
+    resources = @ResourceHint(patterns = {"db/data/*.sql", "db/schema/*.sql"}))
 public class VersionDataSourceScriptHints implements NativeConfiguration {
 
-  @Override
-  public boolean isValid(AotOptions aotOptions) {
-    return NativeConfiguration.super.isValid(aotOptions);
-  }
-
-  @Override
-  public void computeHints(NativeConfigurationRegistry registry, AotOptions aotOptions) {
-    log.info("start read sql script: db/*/*.sql");
-    System.out.println("start read sql script: db/data/*.sql, db/schema/*.sql");
-    registry.resources().add(NativeResourcesEntry.of("db/data/*.sql"))
-        .add(NativeResourcesEntry.of("db/schema/*.sql"));
-  }
 }
