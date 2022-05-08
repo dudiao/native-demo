f391cc68a7ec86c7901e5b31bb5ff6ee68019cb3
songyinyin <song_yinyin@163.com>
2022-05-08 22:56:31 +0800

    add erupt

--- src/main/java/cn/nboot/nativex/demo/NativeDemoApplication.java
+++ src/main/java/cn/nboot/nativex/demo/NativeDemoApplication.java
@@ -2,7 +2,11 @@ package cn.nboot.nativex.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import xyz.erupt.core.annotation.EruptScan;
 
+@EruptScan
+@EntityScan
 @SpringBootApplication
 public class NativeDemoApplication {
 
--- src/main/java/cn/nboot/nativex/demo/controller/IndexController.java
+++ src/main/java/cn/nboot/nativex/demo/controller/IndexController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 public class IndexController {
 
-  @GetMapping("/")
+  @GetMapping("/time")
   public String index() {
     return String.format("success! time %s", LocalDateTime.now());
   }
